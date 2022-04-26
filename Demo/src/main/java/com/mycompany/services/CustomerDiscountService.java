96db833cdb2a23bad10fb32bd237637854730c8d
xianchenw <1951052052hien@ou.edu.vn>
2022-04-26 22:46:58 +0700

    up

---
+++ Demo/src/main/java/com/mycompany/services/CustomerDiscountService.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.services;
+
+import com.mycompany.conf.jdbcUtils;
+import com.mycompany.pojo.Customer;
+import com.mycompany.pojo.CustomerDiscount;
+import com.mycompany.pojo.Discount;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author HIEN
+ */
+public class CustomerDiscountService {
+    public boolean isBirthday(Customer customer){
+        boolean kq = false;
+        try{
+            Timestamp today = Timestamp.valueOf(LocalDateTime.now());
+            Timestamp cusBirthday = Timestamp.valueOf(customer.getBirthday()+" 00:00:00.0");
+            System.out.println(today.getDate()+"/"+today.getMonth());
+            System.out.println(cusBirthday.getDate()+"/"+cusBirthday.getMonth());
+            if(today.getDate()==cusBirthday.getDate()&&today.getMonth()==cusBirthday.getMonth())
+                kq = true;
+        }
+        catch(Exception ex){
+            System.out.print(ex.getMessage());
+        }
+        return kq;
+    }
+    
+    public void addCustomerDiscount(int customerId, int discountId){
+        try(Connection conn = jdbcUtils.getConn()){
+            conn.setAutoCommit(false);
+            PreparedStatement stm = conn.prepareStatement("INSERT INTO customer_discount(customer_id, discount_id) VALUES(?, ?)");
+            stm.setInt(1, customerId);
+            stm.setInt(2, discountId);
+            
+            stm.executeUpdate();
+            
+            conn.commit();
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public Discount getDiscountByName(String name){
+        Discount d = new Discount();
+        try(Connection conn = jdbcUtils.getConn()){
+            PreparedStatement stm = conn.prepareStatement("SELECT * FROM discount WHERE name LIKE ?");
+            stm.setString(1, name);
+            
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()){
+                d.setId(rs.getInt("id"));
+                d.setName(rs.getString("name"));
+                d.setStartTime(rs.getTimestamp("start_time"));
+                d.setEndTime(rs.getTimestamp("end_time"));
+                d.setReducePercentage(rs.getFloat("reduce_percentage"));
+            }
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return d;
+    }
+    
+    public Discount getDiscountById(int id){
+        Discount d = new Discount();
+        try(Connection conn = jdbcUtils.getConn()){
+            PreparedStatement pstm = conn.prepareStatement("SELECT * FROM discount WHERE id LIKE ?");
+            pstm.setInt(1, id);
+            ResultSet rs = pstm.executeQuery();
+            while(rs.next()){
+                d.setId(rs.getInt("id"));
+                d.setName(rs.getString("name"));
+                d.setStartTime(rs.getTimestamp("start_time"));
+                d.setEndTime(rs.getTimestamp("end_time"));
+                d.setReducePercentage(rs.getFloat("reduce_percentage"));
+            }
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        if(d.getStartTime().before(Timestamp.from(Instant.now()))&&
+                    d.getEndTime().after(Timestamp.from(Instant.now())))
+            return d;
+        else
+            return new Discount();
+    }
+}
--- Demo/src/test/java/com/mycompany/receipt/ReceiptTester.java
+++ Demo/src/test/java/com/mycompany/receipt/ReceiptTester.java
@@ -5,18 +5,28 @@
 package com.mycompany.receipt;
 
 import com.mycompany.conf.jdbcUtils;
+import com.mycompany.pojo.Customer;
+import com.mycompany.pojo.Product;
 import com.mycompany.pojo.Receipt;
+import com.mycompany.pojo.ReceiptDetail;
+import com.mycompany.pojo.Store;
 import com.mycompany.pojo.TableReceiptDetailData;
+import com.mycompany.services.CustomerDiscountService;
+import com.mycompany.services.CustomerService;
+import com.mycompany.services.EmployeeService;
 import com.mycompany.services.ProductService;
 import com.mycompany.services.ReceiptDetailService;
 import com.mycompany.services.ReceiptService;
 import com.mycompany.services.StoreProductService;
+import com.mycompany.services.StoreService;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.AfterAll;
@@ -82,32 +92,41 @@ public class ReceiptTester {
         System.out.println(ps.getProductPrice(ps.getProductById(8)));
     }
     
+//    @Test
+//    public void testAddReceipt() throws SQLException{
+//        ReceiptService rs = new ReceiptService();
+//        ProductService ps = new ProductService();
+//        ReceiptDetailService rds = new ReceiptDetailService();
+//        StoreProductService sps = new StoreProductService();
+//        List<TableReceiptDetailData> lt = new ArrayList<>();
+//        rds.addRowProductData(ps.getProductById(1), lt);
+//        rds.addRowProductData(ps.getProductById(2), lt);
+//        rds.addRowProductData(ps.getProductById(3), lt);
+//        rds.addRowProductData(ps.getProductById(8), lt);
+//        rds.addRowProductData(ps.getProductById(8), lt);
+//        rds.addRowProductData(ps.getProductById(5), lt);
+//        rs.addReceipt(Timestamp.valueOf(LocalDateTime.now()),300000 , 2, 1, 1, lt);
+////        List<ReceiptDetail> rd = rds.getReceiptDetailsByReceiptId(31);
+////        sps.updateProductQuantity(rd, 1);
+////        for(ReceiptDetail r:rd){
+////            System.out.println(r.getProductId());
+////            System.out.println(r.getQuantity());
+////        }
+//        System.out.println(Timestamp.valueOf(LocalDateTime.now()));
+//        System.out.println("Hóa đơn");
+////        System.out.println(rs.getLastReceipt().getId());
+//    }
+   
     @Test
-    public void testAddReceipt() throws SQLException{
-        ReceiptService rs = new ReceiptService();
-        ProductService ps = new ProductService();
-        ReceiptDetailService rds = new ReceiptDetailService();
-        StoreProductService sps = new StoreProductService();
-        List<TableReceiptDetailData> lt = new ArrayList<>();
-        rds.addRowProductData(ps.getProductById(1), lt);
-        rds.addRowProductData(ps.getProductById(2), lt);
-        rds.addRowProductData(ps.getProductById(3), lt);
-        rds.addRowProductData(ps.getProductById(8), lt);
-        rds.addRowProductData(ps.getProductById(8), lt);
-        rds.addRowProductData(ps.getProductById(5), lt);
-        rs.addReceipt(Timestamp.valueOf(LocalDateTime.now()),300000 , 2, 1, lt);
-//        List<ReceiptDetail> rd = rds.getReceiptDetailsByReceiptId(31);
-//        sps.updateProductQuantity(rd, 1);
-//        for(ReceiptDetail r:rd){
-//            System.out.println(r.getProductId());
-//            System.out.println(r.getQuantity());
-//        }
-        System.out.println(Timestamp.valueOf(LocalDateTime.now()));
-        System.out.println("Hóa đơn");
-//        System.out.println(rs.getLastReceipt().getId());
+    public void testCusDis(){
+        CustomerDiscountService cds = new CustomerDiscountService();
+        EmployeeService es = new EmployeeService();
+        CustomerService cs = new CustomerService();
+        Customer customer = cs.getCustomerById(7);
+        Timestamp today = Timestamp.valueOf(LocalDateTime.now());
+        Timestamp cusBirthday = Timestamp.valueOf(customer.getBirthday()+" 00:00:00.0");
+        System.out.println(today.getDate()+"/"+today.getMonth());
+        System.out.println(cusBirthday.getDate()+"/"+cusBirthday.getMonth());
+        System.out.println(cds.isBirthday(customer));
     }
-    
-    
-    
-
 }
